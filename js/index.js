0019f7bc4d658e5677737005c21963b0db42b5dd
Romanovalm85@gmail.com <you@example.com>
2020-11-04 23:16:09 +0200

    hw-11

---
+++ js/index.js
@@ -0,0 +1,41 @@
+const colors = [
+  "#FFFFFF",
+  "#2196F3",
+  "#4CAF50",
+  "#FF9800",
+  "#009688",
+  "#795548",
+];
+
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+const refs = {
+  btnStart: document.querySelector('button[data-action="start"]'),
+  btnStop: document.querySelector('button[data-action="stop"]'),
+  body: document.querySelector("body"),
+};
+
+const timer = {
+  isActive: false,
+  start() {
+    if (this.isActive) {
+      return;
+    }
+    this.isActive = true;
+    this.switchColors = setInterval(() => {
+      const min = 0;
+      const max = colors.length - 1;
+      let i = randomIntegerFromInterval(min, max);
+      refs.body.style.backgroundColor = colors[i];
+    }, 1000);
+  },
+  stop() {
+    clearInterval(this.switchColors);
+    this.isActive = false;
+  },
+};
+
+refs.btnStart.addEventListener("click", timer.start.bind(timer));
+refs.btnStop.addEventListener("click", timer.stop.bind(timer));
